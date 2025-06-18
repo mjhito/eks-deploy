8e02a6baef8b8d56c1bb36d11b108f472e8304fc
mjhito <maria.hito@snyk.io>
2025-06-18 17:37:08 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.40.0"
+      version = "~> 5.0"
     }
   }
 
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.0.0"
 }
 
 provider "aws" {
