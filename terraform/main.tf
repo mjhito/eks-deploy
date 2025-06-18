4d8fb88015af63fd1736c85157c52da6662b3d85
mjhito <maria.hito@snyk.io>
2025-06-18 17:17:37 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   # access_key = var.access_key
   # secret_key = var.secret_key
 }
-
+data "aws_caller_identity" "current" {}
 # locals {
 #   name   = "juice-shop-cluster"
 #   region = "us-east-1"
